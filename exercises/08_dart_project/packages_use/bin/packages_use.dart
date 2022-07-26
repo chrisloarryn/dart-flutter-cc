d435af27ba8cfc6a5b08afd6618972d3519e8cb9
chrisloarryn <chrisloarryn@gmail.com>
2022-07-26 01:29:17 -0400

    feat: http and homework

--- exercises/08_dart_project/packages_use/bin/packages_use.dart
+++ exercises/08_dart_project/packages_use/bin/packages_use.dart
@@ -1,5 +1,5 @@
 import 'package:packages_use/packages_use.dart' as packages_use;
 
 void main(List<String> arguments) {
-  print('Hello world: ${packages_use.calculate()}!');
+  packages_use.getReqRespSVC();
 }
--- exercises/08_dart_project/packages_use/lib/packages_use.dart
+++ exercises/08_dart_project/packages_use/lib/packages_use.dart
@@ -1,3 +1,32 @@
-int calculate() {
-  return 6 * 7;
+// int calculate() {
+//   int a = 10;
+//   int b = 20;
+//   a = 30;
+//   b = a + b;
+//   return 6 * 7 * b * a;
+// }
+import 'package:http/http.dart' as http;
+
+import 'package:packages_use/classes/reqres_response.dart';
+
+void getReqRespSVC() {
+  var url = Uri.https('reqres.in', '/api/users', {'page': '2'});
+
+  // var urlParsed = Uri.parse('https://reqres.in/api/users?page=2');
+
+  http.get(url).then((res) {
+    // print(value);
+    // print('object: ${value}');
+    final resReqRes = reqResResponseFromJson(res.body);
+    resReqRes.data.forEach((element) {
+      print(element.firstName);
+      print(element.email);
+    });
+
+    print('page: ${resReqRes.page}');
+    print('per_page: ${resReqRes.perPage}');
+    print('total: ${resReqRes.total}');
+    print('total_pages: ${resReqRes.totalPages}');
+    print('data third element: ${resReqRes.data[2].id}');
+  });
 }
